e61517a90e010a315ceaa8f9e0492221fbd11919
Stefan Kühn <ducdebreme@gmail.com>
2014-10-24 20:53:51 +0100

    developed a slim debian wheezy / apache image for docker

---
+++ lamp55-slim/scripts/start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+##
+# Copy files into specific directories.
+##
+
+# apache
+if [ -f '/etc/conf/apache/vhost.conf' ]; then
+  rm -f /etc/apache2/sites-enabled/*
+  cp /etc/conf/apache/vhost.conf /etc/apache2/sites-available/drupal
+  a2ensite drupal
+fi
+
+# php
+if [ -f '/etc/conf/php/php.ini' ]; then
+  cp /etc/conf/php/php.ini /etc/php5/apache2/php.ini
+  cp /etc/conf/php/php.ini /etc/php5/cli/php.ini
+fi
+
+# make php file executable
+find /var/www/ -name '*.php' -exec chmod +x {} \;
+
+# start apache
+/usr/sbin/apache2ctl -D FOREGROUND
