8383b395f358e3eb04a8d5a5a131db20f8c492bf
Stefan Kühn <ducdebreme@gmail.com>
2014-10-28 07:04:14 +0100

    fixed issues and created image

--- solr4-slim/scripts/start.sh
+++ solr4-slim/scripts/start.sh
@@ -5,7 +5,7 @@
 ##
 
 # copy custom solr index over default index
-if [ -f '/etc/conf/solr-index/*' ]; then
+if [ -d '/etc/conf/solr-index/' ]; then
   cp -R /etc/conf/solr-index/* /var/solr/example/solr/collection1/conf
 fi
 
